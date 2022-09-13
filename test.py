6e80f1fb10df5746271955b50a8ab022cd3f48cf
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-13 09:25:03 -0400

    update

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
-from contextlib import ExitStack
-import time
 import random
-from tkinter import *
-from PIL import ImageTk, Image
-
-# Create App Layout
-# Lottery = Tk()
-# Lottery.geometry('800x360')
-# Lottery.title('Lottery Number Generator')
-# Lottery.iconbitmap("ticket.ico")
-
-# 4 numbers per 3 slots use 3 list to store the numbers
-
-
+import time     
 class Existing_Lottery:
         
      def lottery_info():
@@ -45,24 +32,6 @@ class Existing_Lottery:
         print("Your new numbers are: {} {} {}".format(random.choice(number_slot_1), random.choice(number_slot_2), random.choice(number_slot_3)))
         print("Good Luck, with your new numbers!")
 
-class Lottery:
-
-    def random_generator(): 
-        a, b, c, d = random.randint(0, 9), random.randint(0, 9), random.randint(0, 9), random.randint(0, 9) 
-        print("Random Number Generator is running!")
-        print(f"Your random numbers are: {a} {b} {c} {d} \n", end="", flush=True, file=open("output.txt", "a"), sep=" ")
-
-    def fix_generator():
-        a, b, c, d = random.randint(0, 9), random.randint(0, 9), random.randint(0, 9), random.randint(0, 9) 
-        print("Fix Number Generator is running!")
-        counter = random.randint(0, 9)
-        a, b , c , d = ({(a + counter) /2} , {(b + counter)/2} , {(c + counter) / 2} , {(d + counter)/2})
-        print(f"Your fix numbers are: {a} {b} {c} {d} \n", end="", flush=True, file=open("fixput.txt", "a"), sep=" ")
-
-
 "__init__" == "__main__ "
-# Lottery() 
-# Lottery.random_generator()
-# Lottery.fix_generator()
+# Lottery()
 Existing_Lottery.lottery_info()
-# Lottery.fix_generator()
